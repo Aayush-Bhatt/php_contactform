e1bcee7baadb229f97e474194119b0d394db087a
Aayush Bhatt <aayushbhatt20@gmail.com>
2021-07-01 23:19:33 +0530

    Update send_contact_mail.php

--- send_contact_mail.php
+++ send_contact_mail.php
@@ -5,3 +5,12 @@ if(!empty($_POST["send"])) {
 	$subject = $_POST["subject"];
 	$content = $_POST["content"];
 
+	$toEmail = "admin@phppot_samples.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "contact-view.php";
+?>
